c06fb7f4fdcf856b03970e22420cbd7179165b38
yamamoto-yuta <automatuX78@gmail.com>
2022-04-03 02:19:00 +0900

    💄 UIを作成

--- slack-url-converter/src/popup/Popup.tsx
+++ slack-url-converter/src/popup/Popup.tsx
@@ -1,4 +1,6 @@
-import { TextField } from "@mui/material";
+import { faArrowDown, faArrowsRotate, faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, TextField } from "@mui/material";
 import React, { useEffect } from "react";
 import "./Popup.scss";
 
@@ -10,12 +12,33 @@ export default function Popup() {
 
   return (
     <div className="popupContainer">
-      <TextField
-        id="workspace-id-slack-com"
-        label="<workspace_id>.slack.com"
-        variant="outlined"
-        sx={{ width: "100%" }}
-      />
+      <div className="flex">
+        <TextField
+          id="workspace-id-slack-com-textfield"
+          label="<workspace_id>.slack.com"
+          variant="outlined"
+          sx={{ width: "100%" }}
+        />
+        <Button
+          variant="contained">
+          <FontAwesomeIcon icon={faArrowsRotate} />
+        </Button>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faArrowDown} />
+      </div>
+      <div className="flex">
+        <TextField
+          id="app-slack-com-textfield"
+          label="app.slack.com"
+          variant="outlined"
+          sx={{ width: "100%" }}
+        />
+        <Button
+          variant="contained">
+          <FontAwesomeIcon icon={faCopy} />
+        </Button>
+      </div>
     </div>
   );
 }
